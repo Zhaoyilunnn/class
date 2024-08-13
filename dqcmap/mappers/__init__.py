8c7c98aebd39638a8f410e405020b20c6034951d
YuChenSSR <chenyuoptics@foxmail.com>
2024-08-14 07:20:44 +0800

    update 2 mappers

--- dqcmap/mappers/__init__.py
+++ dqcmap/mappers/__init__.py
@@ -5,9 +5,13 @@ from dqcmap.circuit_prop import CircProperty
 from dqcmap.controller import ControllerConfig
 
 from .trivial_mapper import TrivialMapper
+from .heuristic_graphpartition_mapper import Heuristic_Mapper
+from .iter_KL_mapper import KL_Mapper
 
 MAPPERS = {
     "trivial": TrivialMapper,
+    "heuristic": Heuristic_Mapper,
+    "KL_partition": KL_Mapper,
 }
 
 
@@ -25,5 +29,6 @@ def mapping(
     **mapper_config,
 ):
     """Partition the coupling_map into subgraphs and prune the edges between subgraphs"""
+    mapper_name = "KL_partition"
     mapper = MapperProvider.get(mapper_name, *(ctrl_conf, circ_prop), **mapper_config)
     return mapper.run()
